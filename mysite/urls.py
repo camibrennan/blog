fc9780972f44a6dea86c91051a0b5f58e2806296
Camelia Brennan <cameliabrennan@gmail.com>
2020-10-22 10:03:15 -0400

    trying to add home page

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('home/', include('blog.urls')),
 ]
