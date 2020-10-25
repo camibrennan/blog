6c4cc4968d650ca41e3d94fe0d37bb903f6f781e
Camelia Brennan <cameliabrennan@gmail.com>
2020-10-25 14:52:27 -0400

    created base template and applied to blog

--- blog/views.py
+++ blog/views.py
@@ -7,15 +7,10 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
 def home(request):
     return render(request,'blog/index.html', {})
 
-
 def github_api(request):
-    # We can also combine Django with APIs. This code does an API request, and
-    # then renders the HTML template with the response, every time we visit
-    # this view.
     response = requests.get('https://api.github.com/users/camibrennan/repos')
     repos = response.json()
     context = {
